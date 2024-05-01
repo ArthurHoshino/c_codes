abdb5e2ace293d7904458b16391e772a4ccf0ee3
arthur <arthur.hoshino@gmail.com>
2024-05-01 14:34:46 -0300

    Arquivo trabalho: fila banco v2

--- listaRestrita/trabalhoFila.c
+++ listaRestrita/trabalhoFila.c
@@ -5,6 +5,7 @@
     Guilherme Silva Piantamar: 638205
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node {
     int senha;
@@ -19,17 +20,26 @@ char textosOpt[10][50] = {
     "Informe o tipo da senha (D/I/G/N) >> ", // 0
     "Lista criada\n", // 1
     "A lista esta vazia\n", // 2
-    "A lista esta cheia\n" // 3
+    "A lista esta cheia\n", // 3
     "Exibir lista\n", // 4
     "Lista zerada\n", // 5
-    "Valor adicionado com sucesso\n", // 6
-    "Valor removido com sucesso\n", // 7
+    "Senha emitida\n", // 6
+    "Cliente atendido\n", // 7
     "Deu algum erro\n" // 8
 };
 
 /*
     <=== UTILITARIO ===>
 */
+const char * identificarSO() {
+    #ifdef _WIN32
+        return "cls";
+    #elif __unix__
+        return "clear";
+    #endif
+        return "cls";
+}
+
 void textoStatus(int opt) {
     printf("%s", textosOpt[opt]);
 }
@@ -40,7 +50,7 @@ char pegarTipoSenha() {
     textoStatus(0);
     
     do {
-        scanf("%c", &tipoSenha);
+        scanf(" %c", &tipoSenha);
 
         if (tipoSenha == 'D' ||
             tipoSenha == 'I' ||
@@ -63,43 +73,53 @@ char pegarTipoSenha() {
 void iniciar(FilaSenha **fila) { *fila = NULL; }
 
 int isCheia(FilaSenha **fila) {
-    if ( ( (*fila)->fim->senha % 1000 ) == 999 ) return 1;
-    else return 0;
+    if (*fila != NULL) {
+        if ( (*fila)->fim->senha % 1000 == 999 ) return 1;
+    }
+    return 0;
 }
 
 int isVazia(FilaSenha **fila) {
-    if ((*fila)->prox == NULL) return 1;
+    if (*fila == NULL) return 1;
     else return 0;
 }
 
-void adicionarFim(FilaSenha **fila) {
+void adicionarFim(FilaSenha **fila, char tipoSenha) {
     if (!isCheia(fila)) {
         FilaSenha *novaSenha = (FilaSenha *) malloc (sizeof(FilaSenha));
-        iniciar(&novaSenha);
 
-        novaSenha->senha = (*fila)->fim->senha + 1;
         novaSenha->fim = NULL;
 
         if (isVazia(fila)) {
+            novaSenha->senha = (tipoSenha == 'D') ? 4000 : (tipoSenha == 'I') ? 3000 : (tipoSenha == 'G') ? 2000 : 1000;
             novaSenha->prox = NULL;
-            (*fila)->prox = novaSenha;
-        } else (*fila)->fim->prox = novaSenha;
+            *fila = novaSenha;
+        } else {
+            novaSenha->senha = (*fila)->fim->senha + 1;
+            (*fila)->fim->prox = novaSenha;
+        }
 
         (*fila)->fim = novaSenha;
+        textoStatus(6);
     } else textoStatus(8);
 }
 
-void removerInicio(FilaSenha **fila) {
-    if (!isVazia(fila)) {
-        FilaSenha *tmp = *fila, *guardaValor = (*fila)->fim;
+void removerInicio(FilaSenha **fila[4]) {
+    for (int i = 0; i < 4; i++) {
+        if (!isVazia(fila[i])) {
+            FilaSenha *tmp = *fila[i];
 
-        while (tmp->prox != (*fila)->fim) tmp = tmp->prox;
-        tmp->prox = NULL;
+            if (tmp->prox == NULL) iniciar(fila[i]);
+            else {
+                *fila[i] = tmp->prox;
+                (*fila[i])->fim = tmp->fim;
+            }
 
-        free(guardaValor);
-        (*fila)->fim = tmp;
-
-    } else textoStatus(8);
+            free(tmp);
+            textoStatus(7);
+            return;
+        }
+    }
 }
 
 void exibir(FilaSenha **fila[4]) {
@@ -112,29 +132,42 @@ void exibir(FilaSenha **fila[4]) {
                 printf("|%c%d| -> ", letraSenha, tmp->senha);
                 tmp = tmp->prox;
             }
-            printf("\n");
         }
     }
+}
+
+void limparLista(FilaSenha **fila[4]) {
+    for (int i = 0; i < 4; i++) {
+        if (!isVazia(fila[i])) {
+            FilaSenha *tmp = *fila[i], *guardaValor;
+
+            while (tmp != NULL) {
+                guardaValor = tmp->prox;
+                free(tmp);
+                tmp = guardaValor;
+            }
+        }
+    }
+    textoStatus(5);
 }
 
 /*
     <=== MENU ===>
 */
-void opcao(FilaSenha **deficiente, FilaSenha **idoso, FilaSenha **gestante, FilaSenha **normal, int opcao) {
+void opcao(FilaSenha **filaArray[4], int opcao) {
     char tipoSenha;
     switch (opcao) {
         case 0:
+            limparLista(filaArray);
             break;
         case 1:
             tipoSenha = pegarTipoSenha();
-            adicionarFim( (tipoSenha == 'D') ? deficiente : (tipoSenha == 'I') ? idoso : (tipoSenha == 'G') ? gestante : normal );
+            adicionarFim( (tipoSenha == 'D') ? filaArray[0] : (tipoSenha == 'I') ? filaArray[1] : (tipoSenha == 'G') ? filaArray[2] : filaArray[3], tipoSenha );
             break;
         case 2:
-            tipoSenha = pegarTipoSenha();
-            removerInicio( (tipoSenha == 'D') ? deficiente : (tipoSenha == 'I') ? idoso : (tipoSenha == 'G') ? gestante : normal );
+            removerInicio(filaArray);
             break;
-        case 99:
-            FilaSenha **filaArray[4] = {deficiente, idoso, gestante, normal};
+        case 3:
             exibir(filaArray);
             break;
         default:
@@ -143,17 +176,17 @@ void opcao(FilaSenha **deficiente, FilaSenha **idoso, FilaSenha **gestante, Fila
     }
 }
 
-int menu() {
+int menu(const char* tipoSO) {
     int opt;
 
     printf("\n\nOpcao\n"
             "0. Sair\n"
             "1. Emitir senha\n"
             "2. Atender cliente\n"
-            "99. Exibir\n"
+            "3. Exibir\n"
             ">> ");
     scanf("%d", &opt);
-    system("clear");
+    system(tipoSO);
     return opt;
 }
 
@@ -162,6 +195,7 @@ int main() {
     *filaDeficiente = (FilaSenha *) malloc (sizeof(FilaSenha)),
     *filaIdoso = (FilaSenha *) malloc (sizeof(FilaSenha)),
     *filaNormal = (FilaSenha *) malloc (sizeof(FilaSenha));
+    const char *tipoSO = identificarSO();
     int opt;
     
     iniciar(&filaDeficiente);
@@ -169,8 +203,10 @@ int main() {
     iniciar(&filaGestante);
     iniciar(&filaNormal);
 
+    FilaSenha **filaArray[4] = {&filaDeficiente, &filaIdoso, &filaGestante, &filaNormal};
+
     do {
-        opt = menu();
-        opcao(filaDeficiente, filaIdoso, filaGestante, filaNormal, opt);
+        opt = menu(tipoSO);
+        opcao(filaArray, opt);
     } while (opt);
 }
